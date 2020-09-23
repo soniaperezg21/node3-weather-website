6d7bd27f3e48663aa3fdfb05b666b3a5cf8c946e
soniaperezg21 <soniaperezg21@gmail.com>
2020-09-23 09:48:10 -0500

    forecast humidy

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,18 +7,21 @@ const forecast = (latitude, longitude, callback) => {
     //request({url: url,  json: true}, (error, response) => {
     request({url,  json: true}, (error, {body}) => { //No necesario igualar url valor = name, solo usamos response.body
         if (error) {  //Cuando no se puede conectar a internet o ese tipo de errores
-        callback('Unable to connect to weather service!', undefined) //si ponemos valor en error y nada en data
+             callback('Unable to connect to weather service!', undefined) //si ponemos valor en error y nada en data
         } else if (body.error) {  
             callback('Unable to find location.', undefined)
             //console.log('url-->' + url)
         } else {
+           // console.log(body.current)
             callback(undefined, 
                 body.current.weather_descriptions[0] +
                  '. It is currently ' + body.current.temperature +
-                 ' degrees out. It feels like  ' + body.current.feelslike + ' degrees out.'
+                 ' degrees out. It feels like  ' + body.current.feelslike + ' degrees out. The humidity is ' + 
+                 body.current.humidity + '%.'
             )
          }    
     })
 }
+ 
 
 module.exports = forecast;
